915e5d6273381eef7c7b7b5c7dc0c893d34a0b55
mcoc97616 <mcoc97616@gmail.com>
2025-03-06 07:46:46 +0000

    fiyatlar.dart olustu ve resımler olusturuldu

---
+++ lib/ Epın.dart
@@ -0,0 +1,17 @@
+class fiyatlar{
+  String urunadi;
+  String urunfotografi;
+
+  fiyatlar(this.urunadi,this.urunfotografi);
+  static List<fiyatlar> urunler=[
+    fiyatlar("150VP"," 1.png "),
+    fiyatlar("600VP"," 2.png "),
+    fiyatlar("1200VP"," 3.png "),
+    fiyatlar("2200VP","4.png "),
+    fiyatlar("3500VP","5.png "),
+
+  ];
+
+}
+
+
